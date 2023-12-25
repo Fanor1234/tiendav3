904cd2a00a0003bbea957aa0f20095feb028d731
Fanor1234 <luis_faurp@hotmail.com>
2023-12-25 12:01:22 -0400

    fua

--- ecom/settings.py
+++ ecom/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
-STATIC_URL = BASE_DIR/"sttic_files_build"/"static"
+STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
